ac36d28acc0ae943aeb2ae1718cc6befb61c1b70
salvorhlm01 <salvodamore2301@gmail.com>
2021-10-20 17:27:37 +0200

    added logo in assets

--- WordFit/ContentView.swift
+++ WordFit/ContentView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack{
-            Text("Word Fit")
+            Image("logo")
+//            Text("Word Fit")
             ContentView2()
         }
     }
