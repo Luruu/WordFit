ff28b5f73189d5d2a349643083e23d5e87ae2506
Renato Esposito <53092291+RenatoEsposito1999@users.noreply.github.com>
2021-10-20 17:27:19 +0200

    added word file

--- WordFit/ContentView.swift
+++ WordFit/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack{
-            Text("Word Fit")
+            Text("Wordhhh Fit")
             ContentView2()
         }
     }
