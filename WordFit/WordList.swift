05184063806b9a8701aaf6aca77db9edeeddadc0
Renato Esposito <53092291+RenatoEsposito1999@users.noreply.github.com>
2021-10-21 15:55:40 +0200

    json added

---
+++ WordFit/WordList.swift
@@ -0,0 +1,20 @@
+//
+//  WordList.swift
+//  WordFit
+//
+//  Created by Renato on 21/10/21.
+//
+
+import Foundation
+
+class wordList{
+    private var wordList : Array<Word> = Array.init()
+//  private var lenght : Int = Int.init()
+    
+    init(){}
+    
+    func setWord(path: String){
+        
+    }
+    
+}
