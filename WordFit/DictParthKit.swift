fc5209804b073f87f0020c2d6f601730a483c9df
OlMario99 <48529926+OlMario99@users.noreply.github.com>
2021-10-26 17:22:53 +0200

    same changes1

--- WordFit/DictParthKit.swift
+++ WordFit/DictParthKit.swift
@@ -15,30 +15,10 @@ class DictParthKit{
     static func getIstance() -> DictParthKit {
         return .istance
     }
-    func read(key_:String) -> Dictionary<String, String> {
-        return kit.readSync(team: "TeamF", tag: "Ranking", key: key_)
+    func read(tag_:String, key_:String) -> Dictionary<String, String> {
+        return kit.readSync(team: "TeamF", tag: tag_, key: key_)
     }
-    func write(key_:String, value_:String) -> Bool {
-        return kit.writeSync(team: "TeamF", tag: "Ranking", key: key_, value: value_)
+    func write(tag_:String, key_:String, value_:String) -> Bool {
+        return kit.writeSync(team: "TeamF", tag: tag_, key: key_, value: value_)
     }
-    
-    func ExistsKey(key: String) -> Bool{
-        if read(key_: key).count == 0{
-            print("Uniquekey: nome univoco")
-            return false
-        }
-        else {
-            print("Uniquekey: nome non univoco")
-            return true
-        }
-    }
-    
-        func updateRanking(key : String){
-            write(key_: key, value_: "5000")
-            var dict: [String: String] = [:]
-            dict = read(key_: key)
-            print(dict)
-            
-            
-        }
 }
